476c0c563731d92f7fcc47adbd6134f3760c5f57
Q1000Q <1000q.mj@gmail.com>
2022-11-23 21:46:00 +0100

    Adds script v1.0 and README
    Adds first version of the python script and README file.

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Azure upload all files to blob storage
+This script upload all files in directory where is this script and subdirectories to your azure blob storage container.
+<br />
+<span style="color: red"> IMPORTANT: Change 4 varibles in start of the script to yours blob storage ones </span>
---
+++ push_file.py
@@ -0,0 +1,42 @@
+import os
+from glob import glob
+from azure.storage.blob import BlobServiceClient
+
+storage_account_key = "insert your here"
+storage_account_name = "insert your here"
+connection_string = "insert your here"
+container_name = "insert your here"
+
+files = []
+allfiles = []
+
+
+def uploadToBlobStorage(file_path, file_name):
+    blob_service_client = BlobServiceClient.from_connection_string(connection_string)
+    blob_client = blob_service_client.get_blob_client(container=container_name, blob=file_name)
+    with open(file_path, "rb") as data:
+        blob_client.upload_blob(data, overwrite=True)
+        print(f"Uploaded {file_name}.")
+
+
+dir = "*"
+while True:
+    files = glob(f"{dir}")
+    if not files:
+        break
+    for file in files:
+        if os.path.isfile(file):
+            file = file.replace("\\", "/")
+            allfiles.append(file)
+    dir += "\*"
+
+
+print(f"Files to upload: {allfiles}")
+print("Press Enter to continue...")
+input()
+
+for file in allfiles:
+    uploadToBlobStorage(file, file)
+
+print("All done.")
+input()
